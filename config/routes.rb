8d8205238824ea1e8d26b8077e27282d75bdec8d
johann bzaih <johann.bzaih@gmail.com>
2021-05-18 15:30:32 +0200

    modified navbar with dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'dashboard', to: 'pages#dashboard'
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :cities do
