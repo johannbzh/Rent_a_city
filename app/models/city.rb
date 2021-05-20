ae7ccd2d0e69fae410bacebfe75117b8ff730348
Maxime Jacob <maxime.jacob@me.com>
2021-05-20 11:39:04 +0200

    review model

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,9 +1,10 @@
 class City < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
+  has_many :reviews, dependent: :destroy
   validates :name, presence: true
   validates :country, presence: true
-  validates :price, numericality: { only_integer: true }
+  validates :price, numericality: { only_integer: true }, presence: true
   validates :main_monument, presence: true
   has_many_attached :photos
 end
---
+++ app/models/review.rb
@@ -0,0 +1,6 @@
+class Review < ApplicationRecord
+  belongs_to :city
+  belongs_to :user
+  validates :content, length: { minimum: 5 }, presence: true
+  validates :rating, numericality: { only_integer: true }, inclusion: { in: 0..5 }
+end
