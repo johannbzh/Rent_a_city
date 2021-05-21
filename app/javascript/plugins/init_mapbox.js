3884247e0911a91ff6f8afe79b9afc01427d619f
Chenchen Zheng <chenchenzheng42@gmail.com>
2021-05-21 15:30:24 +0200

    delete placeholder

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -20,13 +20,8 @@ const initMapbox = () => {
       const popup = new mapboxgl.Popup().setHTML(marker.info_window); // add this
       
       const element = document.createElement('div');
-      element.className = 'marker';
-      element.style.backgroundImage = `url('${marker.image_url}')`;
-      element.style.backgroundSize = 'contain';
-      element.style.width = '25px';
-      element.style.height = '25px';
 
-      new mapboxgl.Marker(element)
+      new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
         .setPopup(popup) // add this
         .addTo(map);
